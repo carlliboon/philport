a6a03a8a1a777aebc86fa4c3821a8fbef0b325d3
michael <carlliboon12@gmail.com>
2025-05-22 10:47:45 +0800

    update metadata for Shopify Support Pro in layout and remove redundant metadata from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Home from "../app/home/home";
 
-export const metadata = {
-  title: "Shopify Support Pro - Your Trusted Partner for Shopify Store Success",
-  description:
-    "Shopify Support Pro offers expert assistance for your Shopify store, ensuring smooth operations and enhanced performance.",
-};
-
 export default function Page() {
   // Explicitly type the state as an array of `Review` objects
 
