e4fa020a4a13c02af9908f88c05b2df6bc1ace8d
michael <carlliboon12@gmail.com>
2025-05-27 09:52:38 +0800

    update

--- src/app/admin/dashboard/DashboardClient.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export const DashboardClient = () => {
-  return <div>DashboardClient</div>;
-};
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-export {} from "./DashboardClient";
-
-// Disable static prerender to avoid Firebase during build
-export const dynamic = "force-dynamic";
+export const DashboardClient = () => {
+  return <div>DashboardClient</div>;
+};
